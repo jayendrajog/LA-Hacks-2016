04f57653f90438fe453cb2e0f3631c84f66462e5
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-04-30 17:26:37 -0700

    Implement face_auth verify

--- face_auth.py
+++ face_auth.py
@@ -15,21 +15,33 @@
 import requests
 import json
 
+ANNA_KENDRICK_1 = "http://hellogiggles.com/wp-content/uploads/2015/04/10/anna-kendrick-pitch-perfect-650-430.jpg"
+ANNA_KENDRICK_2 = "http://zntent.com/wp-content/uploads/2015/02/Anna-Kendrick-2.jpg"
+
+# detect and get faceIds
 detectURL = "https://api.projectoxford.ai/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age"
-detectHeaders = {
+FaceAPIHeaders = {
 	"Content-Type": "application/json",
 	"Ocp-Apim-Subscription-Key": "38c44ac804c44f6e97673d815163a1db"
 }
 
-res = requests.post(detectURL, data=json.dumps({"url":"http://hellogiggles.com/wp-content/uploads/2015/04/10/anna-kendrick-pitch-perfect-650-430.jpg"}), headers=detectHeaders)
+res1 = requests.post(detectURL, data=json.dumps({"url":ANNA_KENDRICK_1}), headers=FaceAPIHeaders)
+res2 = requests.post(detectURL, data=json.dumps({"url":ANNA_KENDRICK_2}), headers=FaceAPIHeaders)
 # prints response
 #print(res.content)
 
 # prints faceId
-detectDict = json.loads(res.content)[0]
-print(detectDict["faceId"])
+detectDict1 = json.loads(res1.content)[0]	# for the first line
+print(detectDict1["faceId"])
+detectDict2 = json.loads(res2.content)[0]
+print(detectDict2["faceId"])
 
 
+# verify faceIds
+verifyURL = "https://api.projectoxford.ai/face/v1.0/verify"
+res = requests.post(verifyURL, data=json.dumps({"faceId1":detectDict1["faceId"], "faceId2":detectDict2["faceId"]}), headers=FaceAPIHeaders)
+# prints response
+print(res.content)
 
 
 
@@ -37,7 +49,7 @@ print(detectDict["faceId"])
 
 
 ########################
-NOT USEFUL BELOW HERE
+#NOT USEFUL BELOW HERE
 ########################
 
 
